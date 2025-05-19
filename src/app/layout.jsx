a640f64d92de37245fb090742c8d4220f1e67450
Spectroze <edmarrin05@gmail.com>
2025-05-19 14:33:48 +0800

    Fixing Deployment

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from 'next/font/google';
 import "./globals.css";
 import { Toaster } from "@/components/ui/toast";
 import Logger from '@/lib/logger';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: "DSWD Social Welfare Capital Fund",
@@ -23,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} antialiased`}
       >
         {children}
         <Toaster />
